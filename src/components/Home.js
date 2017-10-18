b0f04cb593649a5d82a50839138808e8bcc95941
Kelsey <kbeffel10@me.com>
2017-10-18 14:48:38 -0700

    initial carousel changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,80 +9,59 @@ class Home extends Component {
         <div>
             
             <div id="main" className="container text-center">
-                <Carousel id="carousel-college">
-                    <Carousel.Item>
-                        <img src="/assets/graduation1.jpg" alt="graduation1"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/amherstcollege.jpg" alt="amherstcollege"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/studentsoutsidedorms.jpg" alt="studentsoutsidedorms"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/harvard-boston.jpg" alt="harvard-boston"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/studentstudying.jpg" alt="studentstudying"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/UWCherries.jpg" alt="UWCherries"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/graduation2.jpg" alt="graduation2"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="/assets/berkeleyuniversity.jpg" alt="berkeleyuniversity"/>
-                        <Carousel.Caption className="caption">
-                            <h1>Welcome to YOUniversity</h1>
-                            <h2>Empowering you to find your DREAM School</h2>
-                            <h2>Login or create your new user account and.....</h2>
-                            <h1>Begin your Academic Adventure</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-            </Carousel>
+
+        <Carousel id="carousel-college">
+            <Carousel.Item>
+                <img src="/assets/images/1_halls.jpg" alt="school halls"/>
+                <Carousel.Caption className="caption">
+                    {<h2>Welcome to</h2>}
+                    <h1>YOUniversity</h1>
+                    {/* <h2>Empowering you to find your DREAM School</h2>
+                    <h2>Login or create your new user account and.....</h2> */}
+                    <h2><i>Your Academic Adventure Starts Here</i></h2>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="/assets/images/2_graduates.jpg" alt="graduates"/>
+                <Carousel.Caption className="caption">
+                    {<h2>Welcome to</h2>}
+                    <h1>YOUniversity</h1>
+                    {/* <h2>Empowering you to find your DREAM School</h2>
+                    <h2>Login or create your new user account and.....</h2> */}
+                    <h2><i>Your Academic Adventure Starts Here</i></h2>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="/assets/images/3_path.jpg" alt="school view"/>
+                <Carousel.Caption className="caption">
+                    {<h2>Welcome to</h2>}
+                    <h1>YOUniversity</h1>
+                    {/* <h2>Empowering you to find your DREAM School</h2>
+                    <h2>Login or create your new user account and.....</h2> */}
+                    <h2><i>Your Academic Adventure Starts Here</i></h2>
+                </Carousel.Caption>
+            </Carousel.Item>
+            {/* <Carousel.Item>
+                <img src="/assets/images/4_library" alt="school library"/>
+                <Carousel.Caption className="caption">
+                    <h1>Welcome to YOUniversity</h1>
+                    <h2>Empowering you to find your DREAM School</h2>
+                    <h2>Login or create your new user account and.....</h2>
+                    <h1> Begin your Academic Adventure</h1>
+                </Carousel.Caption>
+            </Carousel.Item> */}
+            <Carousel.Item>
+                <img src="/assets/images/5_school.jpg" alt="school"/>
+                <Carousel.Caption className="caption">
+                    {<h2>Welcome to</h2>}
+                    <h1>YOUniversity</h1>
+                    {/* <h2>Empowering you to find your DREAM School</h2>
+                    <h2>Login or create your new user account and.....</h2> */}
+                    <h2><i>Your Academic Adventure Starts Here</i></h2>
+                </Carousel.Caption>
+            </Carousel.Item>
+      </Carousel>
+
         </div>
         <div id="marketing" className="container text-center">
             <Grid>
